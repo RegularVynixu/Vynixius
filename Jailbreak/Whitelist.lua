92f510ab74fd9f05d7724fd5454353fda4c690d9
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2021-09-08 06:42:33 +0200

    Update Whitelist.lua

--- Jailbreak/Whitelist.lua
+++ Jailbreak/Whitelist.lua
@@ -1,1 +1,1 @@
-return {"Synapse X", "ScriptWare", "Krnl", "Fluxus", "Electron", "Omega X"}
+return {"Synapse X", "ScriptWare", "Krnl", "Fluxus", "Electron", "Omega X", "Shadow", "Comet", "Trigon", "Skisploit"}

21e5c70d09a6aa4085d729d9b48db1872df444a3
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-01-09 19:52:59 +0100

    Update Compatibility.lua

--- Jailbreak/Compatibility.lua
+++ Jailbreak/Compatibility.lua
@@ -1,2 +1,16 @@
--- it's just to keep the skids out lmfao
-return table.find({"Synapse X", "Krnl", "OxygenU", "Comet", "Delta", "Electron"}, ({identifyexecutor()})[1]) and true or false
+return function(bool)
+    local Check = table.find({"Synapse X", "Krnl", "OxygenU", "Comet", "Delta", "Electron"}, ({identifyexecutor()})[1]) and true or false
+  
+    if bool then
+        local Notification = require(game:GetService("ReplicatedStorage").Game.Notification)
+        local Settings = require(game:GetService("ReplicatedStorage").Resource.Settings)
+        local GameUtil = require(game:GetService("ReplicatedStorage").Game.GameUtil)
+        
+        Notification.Gui.ContainerNotification.ImageColor3 = Check and Color3.fromRGB(0, 255, 0) or Color3.fromRGB(255, 0, 0)
+        Notification.new({Text = "Your executor is ".. (not Check and "not " or "").. "supported.", Duration = 5})
+        task.wait(5)
+        Notification.Gui.ContainerNotification.ImageColor3 = Settings.TeamColor[GameUtil.Team]
+    else
+        return Check
+    end
+end

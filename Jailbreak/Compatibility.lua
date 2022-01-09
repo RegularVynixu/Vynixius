0f112f171cda9375dc518bd8090b7ac5c93cec97
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-01-09 16:25:45 +0100

    Update Compatibility.lua

--- Jailbreak/Compatibility.lua
+++ Jailbreak/Compatibility.lua
@@ -1,2 +1,2 @@
 -- it's just to keep the skids out lmfao
-return table.find({"Synapse X", "Krnl", "OxygenU", "Comet"}, ({identifyexecutor()})[1]) and true or false
+return table.find({"Synapse X", "Krnl", "OxygenU", "Comet", "Delta", "Electron"}, ({identifyexecutor()})[1]) and true or false

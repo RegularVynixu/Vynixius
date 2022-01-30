4699ce58fd994f6eb0321a3990df37b3769f42e9
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-01-30 15:13:09 +0100

    Update Locations.lua

--- Prison Life/Locations.lua
+++ Prison Life/Locations.lua
@@ -1,47 +1,14 @@
--- Services
-
-local Players = game:GetService("Players")
-
--- Variables
-
-local Plr = Players.LocalPlayer
-local Cell
-
--- Scripts
-
-for i, v in next, workspace.Prison_Cellblock:GetDescendants() do
-    if v:FindFirstChild("Occupant") and v.Occupant.Value == Plr then
-        Cell = v
-        break
-    end
-end
-
-local Locations = {
-    Names = {"Cell Block", "Cafeteria", "Guard Area", "Prison Yard", "Prison Garage", "Prison Entrance", "Criminal Base", "City"},
+return {
+    Names = {"Cell", "Cell Block", "Cafeteria", "Guard Area", "Prison Yard", "Prison Garage", "Prison Entrance", "Criminal Base", "City"},
     Locations = {
+        ["Cell"],
         ["Cell Block"] = CFrame.new(916, 100, 2448),
-        ["Cafeteria"] = CFrame.new(884, 101, 2288),
+        ["Cafeteria"] = CFrame.new(933, 98, 2289),
         ["Guard Area"] = CFrame.new(834, 100, 2316),
         ["Prison Yard"] = CFrame.new(779, 99, 2472),
-        ["Prison Garage"] = CFrame.new(570, 99, 2469),
+        ["Prison Garage"] = CFrame.new(616, 96, 2508),
         ["Prison Entrance"] = CFrame.new(468, 99, 2252),
         ["Criminal Base"] = CFrame.new(-921, 94, 2191),
         ["City"] = CFrame.new(-361, 56, 1734),
     },
 }
-
-if Cell then -- Some gay shit for when the cells somehow break /shrug
-    for i = 1, #Locations.Names do
-        local idx = #Locations.Names - i
-        Locations.Names[idx + 1] = Locations.Names[idx]
-    end
-    for i = 1, #Locations.Locations do
-        local idx = #Locations.Locations - i
-        Locations.Locations[idx + 1] = Locations.Locations[idx]
-    end
-    
-    Locations.Names[1] = "Cell"
-    Locations.Locations[Locations.Names[1]] = Cell.spawnbrick.CFrame
-end
-
-return Locations

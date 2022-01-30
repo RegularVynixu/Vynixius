c02f6d41df46837c1ee62b205212409991aa249b
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-01-30 15:28:32 +0100

    Update Locations.lua

--- Prison Life/Locations.lua
+++ Prison Life/Locations.lua
@@ -1,7 +1,7 @@
 return {
     Names = {"Cell", "Cell Block", "Cafeteria", "Guard Area", "Prison Yard", "Prison Garage", "Prison Entrance", "Criminal Base", "City"},
     Locations = {
-        ["Cell"] = CFrame.new(0, 0, 0),
+        ["Cell"] = CFrame.new(),
         ["Cell Block"] = CFrame.new(916, 100, 2448),
         ["Cafeteria"] = CFrame.new(933, 98, 2289),
         ["Guard Area"] = CFrame.new(834, 100, 2316),

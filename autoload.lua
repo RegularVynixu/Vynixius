fbf986d6af2c20374a1deb5899b0d7e7e1cf90c3
RoCord <57117549+rocord01@users.noreply.github.com>
2022-02-08 01:05:27 -0500

    Added auto-loading script
    Added a script that runs the Vynixius script related to that game automatically.

---
+++ autoload.lua
@@ -0,0 +1,13 @@
+local PlaceId = game.PlaceId
+sleep(10)
+if PlaceId == 606849621 then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Jailbreak/Script.lua")()
+elseif PlaceId == 537413528 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/BABFT"))()
+elseif PlaceId == 4623386862 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Piggy"))()
+elseif PlaceId == 155615604 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Prison%20Life/Script.lua"))()
+elseif PlaceId == 1962086868 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Tower%20of%20Hell/Tower%20of%20Hell")()
+end

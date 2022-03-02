977ff4209f9694b6f8b19c12b5cc66fa4e04aa3f
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-03-02 20:20:43 +0100

    Create Advertisements.lua

---
+++ Jailbreak/Advertisements.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        name = "Oxygen U",
+        data = {
+            Image = "https://cdn.discordapp.com/attachments/923279410608619520/937799314846134312/IMG_2622.png",
+            Invite = "https://discord.com/invite/3f7f2JK3Rc",
+        },
+        properties = {
+            Size = UDim2.new(0, 485, 0, 60),
+        },
+    },
+    {
+        name = "Fluxus",
+        data = {
+            Image = "https://media.discordapp.net/attachments/921876319057375273/938529994408353832/New_Project_13D6D9C.png?width=1328&height=747",
+            Invite = game:HttpGet("https://fluxteam.xyz/external-files/discord.php"),
+        },
+        properties = {
+            Size = UDim2.new(0, 332, 0, 187),
+        },
+    },
+}

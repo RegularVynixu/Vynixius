c84e77ea8829605a063a09d2c0f2e7ac569dc275
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-05-19 23:23:08 +0200

    Update Compatibility.lua

--- Jailbreak/Compatibility.lua
+++ Jailbreak/Compatibility.lua
@@ -1,32 +1,34 @@
-local Functions = {
-    getgc,
-    getfenv,
-    islclosure,
-    newcclosure,
-    getconnections,
-    firesignal,
-    hookmetamethod,
-    checkcaller,
-    getnamecallmethod,
+return function() -- temporary so the current shit doesn't break
+    local Functions = {
+        getgc,
+        getfenv,
+        islclosure,
+        newcclosure,
+        getconnections,
+        firesignal,
+        hookmetamethod,
+        checkcaller,
+        getnamecallmethod,
 
-    debug.getproto,
-    debug.getupvalue,
-    debug.getupvalues,
-    debug.setupvalues,
-    debug.getconstant,
-    debug.getconstants,
-    debug.setconstant,
-    debug.getinfo,
-    debug.traceback,
-    
-    is_synapse_function or iskrnlclosure or isexecutorclosure,
-    (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
-    (syn and syn.request) or http_request or request,
-}
+        debug.getproto,
+        debug.getupvalue,
+        debug.getupvalues,
+        debug.setupvalues,
+        debug.getconstant,
+        debug.getconstants,
+        debug.setconstant,
+        debug.getinfo,
+        debug.traceback,
 
-for i, v in next, Functions do
-    if not v then
-        return false
+        is_synapse_function or iskrnlclosure or isexecutorclosure,
+        (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
+        (syn and syn.request) or http_request or request,
+    }
+
+    for i, v in next, Functions do
+        if not v then
+            return false
+        end
     end
+    return true
 end
-return true

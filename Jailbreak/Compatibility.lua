860e46660f2b1a4f02de1da5c97d587c88b477ab
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-06-11 23:20:56 +0200

    Update Compatibility.lua

--- Jailbreak/Compatibility.lua
+++ Jailbreak/Compatibility.lua
@@ -1,34 +1,3 @@
-return function() -- temporary so the current shit doesn't break
-    local Functions = {
-        getgc,
-        getfenv,
-        islclosure,
-        newcclosure,
-        getconnections,
-        firesignal,
-        hookmetamethod,
-        checkcaller,
-        getnamecallmethod,
-
-        debug.getproto,
-        debug.getupvalue,
-        debug.getupvalues,
-        debug.setupvalues,
-        debug.getconstant,
-        debug.getconstants,
-        debug.setconstant,
-        debug.getinfo,
-        debug.traceback,
-
-        is_synapse_function or iskrnlclosure or isexecutorclosure,
-        (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
-        (syn and syn.request) or http_request or request,
-    }
-
-    for i, v in next, Functions do
-        if not v then
-            return false
-        end
-    end
-    return true
+return function()
+    return table.find({ "Synapse X", "Krnl", "OxygenU", "Comet", "Delta", "Electron", "ScriptWare", "KatoAPI" }, identifyexecutor()) and true or false
 end

51d428e5b817b36a6f5694fb42189a09774ee4d3
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-07-23 19:03:08 +0200

    Update Loader.lua

--- Piggy/Loader.lua
+++ Piggy/Loader.lua
@@ -1,4 +1,8 @@
-loadstring(({
+local selectedBook = ({
     [4623386862] = game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Book_1.lua"),
     [5661005779] = game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Book_2.lua"),
-})[game.PlaceId])()
+})[game.PlaceId]
+
+if selectedBook ~= nil then
+    loadstring(selectedBook)()
+end

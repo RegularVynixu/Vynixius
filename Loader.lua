95f7f67cddfac8857a3d4e86ee4fe5b0817becd7
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-07-23 11:35:22 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,7 +9,7 @@ local scriptUrl = ({
     [537413528] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/BABFT",
     [3101667897] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Legends%20Of%20Speed/Script.lua",
     [3956818381] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Ninja%20Legends/Script.lua",
-    [4623386862] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Piggy",
+    [4623386862] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Script.lua",
     [155615604] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Prison%20Life/Script.lua",
     [1962086868] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Tower%20of%20Hell/Tower%20of%20Hell"
 })[game.PlaceId]

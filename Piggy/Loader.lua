1bbbb72a0ab0a09ee0fa1fe03ee7c6cab765cf3a
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-07-23 19:02:24 +0200

    Create Loader.lua

---
+++ Piggy/Loader.lua
@@ -0,0 +1,4 @@
+loadstring(({
+    [4623386862] = game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Book_1.lua"),
+    [5661005779] = game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Book_2.lua"),
+})[game.PlaceId])()

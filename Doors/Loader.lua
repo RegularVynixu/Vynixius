9b7d06c5fe579759ca3ac2ed6db1d37439ca7f64
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2023-02-10 16:47:00 +0100

    Update Loader.lua

--- Doors/Loader.lua
+++ Doors/Loader.lua
@@ -1,10 +1,10 @@
 if game.PlaceId == 6839171747 then
     local floor = game:GetService("ReplicatedStorage").GameData.Floor.Value
 
-    if floor.Value == "Hotel" then
+    if floor == "Hotel" then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Script.lua"))()
 
-    elseif floor.Value == "Rooms" then
+    elseif floor == "Rooms" then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/The%20Rooms/Script.lua"))()
     end
 end
